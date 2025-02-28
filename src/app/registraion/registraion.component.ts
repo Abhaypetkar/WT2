0b95f17d64199a4083f39393a1fb05671e4cd26c
Abhay <abhaypetkar834@gmail.com>
2025-02-28 15:23:23 +0530

    Assignment 2

---
+++ src/app/registraion/registraion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registraion',
+  imports: [],
+  templateUrl: './registraion.component.html',
+  styleUrl: './registraion.component.css'
+})
+export class RegistraionComponent {
+
+}
